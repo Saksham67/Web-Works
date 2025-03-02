1154646249baf3ad8791adab57c75d60f27614b8
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-03 02:20:03 +0530

    Added new files

---
+++ CelsiusToFahrenheit/script.js
@@ -0,0 +1,9 @@
+function convertTemperature(){
+    let celsius = document.getElementById("celsius").value;
+    if (celsius === "") {
+        document.getElementById("result").innerHTML = "Please enter a temperature.";
+        return;
+    }
+    let fahrenheit =  (parseFloat(celsius) * 9/5) + 32;
+    document.getElementById("result").innerHTML = `Fahrenheit: ${fahrenheit.toFixed(2)}`;
+}
