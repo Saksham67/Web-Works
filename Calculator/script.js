6616c0d7769fa1621657e354f75aea8207e5dc58
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-26 01:51:43 +0530

    added

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,3 @@
+const display = document.getElementById("input");
+
 function display()
-{
-    let Input = document.getElementById("input").value ;
-    document.getElementById("input").innerHTML = Input;
-
-}
-let Operator = document.getElementByIdTagName("button");
-
-
